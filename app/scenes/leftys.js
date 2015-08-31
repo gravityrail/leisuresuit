cda5b573818079508dff5fba12db1fd308faa3e4
Daniel Walmsley <goldsounds@gmail.com>
2015-08-30 20:31:40 -0700

    Abstract out res scaling

--- app/scenes/leftys.js
+++ app/scenes/leftys.js
@@ -1,25 +1,23 @@
+var scale = require('../scale');
+
 function createPlayer() {
 	player = Crafty.e("2D, Canvas, larry, RightControls, Hero, Animate, Collision")
 		.attr({
-			x: 120 * 4,
-			y: 110 * 4,
+			x: scale(120),
+			y: scale(110),
 			z: 1
 		})
 		.rightControls(4);
 }
 
-// Method to randomy generate the map
 function generateWorld() {
-	//debugger;
-	var scale = 4;
 	Crafty.e("Background")
 		.background("bg_leftys_interior")
-		.wall(0, scale * 160, scale * 320, 8)
-		.door('main', scale * 140, scale * 159, scale * 50, scale * 10);
+		.wall(0, scale(160), scale(320), 8)
+		.door('main', scale(140), scale(159), scale(50), scale(10));
 }
 
 module.exports = Crafty.defineScene("leftys", function() {
-	console.log("leftys");
 	generateWorld();
 	createPlayer();
 });
--- app/scenes/main.js
+++ app/scenes/main.js
@@ -1,28 +1,24 @@
-// var Info = require('../interfaces/info');
+var scale = require('../scale');
 
 function createPlayer() {
 	player = Crafty.e("2D, Canvas, larry, RightControls, Hero, Animate, Collision")
 		.attr({
-			x: 120 * 4,
-			y: 110 * 4,
+			x: scale(120),
+			y: scale(110),
 			z: 1
 		})
 		.rightControls(4);
 }
 
-// Method to randomy generate the map
 function generateWorld() {
-	//debugger;
-	var scale = 4;
 	Crafty.e("Background")
 		.background("bg_leftys")
-		.wall(scale * 170, 0, scale * 100, scale * 110)
-		.door('leftys', 0, 0, 4 * 320, scale * 100)
-		.wall(0, 0, 4 * 100, scale * 110);
+		.wall(scale(170), 0, scale(100), scale(110))
+		.door('leftys', 0, 0, 4 * 320, scale(100))
+		.wall(0, 0, scale(100), scale(110));
 }
 
 module.exports = Crafty.defineScene("main", function() {
-	// infc['info'] = new Info();
 	generateWorld();
 	createPlayer();
 });
